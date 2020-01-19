42cb6589db6a9fbd4717aaf0030b708c1a48b85f
Chandra Prakash <cptiwari20@gmail.com>
2020-01-19 15:40:09 +0530

    Added env-cmd and changed IDE

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = () => {
+  return  (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -16,6 +17,6 @@ const IndexPage = () => (
     </div>
     <Link to="/about">Go to About Page</Link>
   </Layout>
-)
+)}
 
 export default IndexPage
