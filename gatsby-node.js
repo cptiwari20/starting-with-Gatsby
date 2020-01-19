aaebf415131c76429b2eb1296d45deb79b22043e
Chandra Prakash <cptiwari20@gmail.com>
2020-01-19 22:23:03 +0530

    created pages based on  markdown slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+// create a node field
 module.exports.onCreateNode = ({node, actions}) => {
     const { createNodeField } = actions;
     if(node.internal.type === 'MarkdownRemark'){
@@ -10,4 +11,33 @@ module.exports.onCreateNode = ({node, actions}) => {
             value: slug
         });
     }
+}
+
+// create pages based on markdown file name
+module.exports.createPages = async ({graphql, actions}) => {
+    const { createPage } = actions;
+    const blogPostTemplate = path.resolve('./src/template/blog.js');
+    const res = await graphql(`
+    query {
+        allMarkdownRemark{
+            edges{
+                node{
+                    fields {
+                        slug
+                    }
+                }
+            }
+        }
+    }`
+    );
+
+      res.data.allMarkdownRemark.edges.forEach(edge => {
+          createPage({
+              path: `/blog/${edge.node.fields.slug}`,
+              component: blogPostTemplate,
+              context: {
+                  slug: edge.node.fields.slug
+              }
+          })
+      });
 }
